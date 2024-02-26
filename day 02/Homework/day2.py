ab4db3801ee297fec2c173a82528ad885b2f4b7b
mateghviniashvili <ghviniashvilimate318@gmail.com>
2024-02-26 20:11:53 +0400

    homework

---
+++ day 02/Homework/day2.py
@@ -0,0 +1,3 @@
+print("me gavicani:data tezelashvili,dato berkacashvili,rati ruxadze")
+print("dzalian kargad mexmarebian")
+print("me vfiqrob goaze rom aris yvelaze kargi akademia cemi ganvitarebistvis")
