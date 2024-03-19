00da3b29b61f50ef254ba367af4055900aaa0f71
mateghviniashvili <ghviniashvilimate318@gmail.com>
2024-03-19 20:55:14 +0400

    Update homework1.py

--- day 07/homework/homework1.py
+++ day 07/homework/homework1.py
@@ -1,12 +1,11 @@
-information = int(input("Enter your information: "))
-information1 = input("Enter your information: ")
+information = input("Enter your information: ")
+information1 = int(input("Enter your information: "))
 information2 = float(input("Enter your information2: "))
-information3 = input(type("Enteryour information3: "))
-
-
-
-
-
+information3 = input("Enter your information3: ")
+print(type(information))
+print(type(information1))
+print(type(information2))
+print(type(information3))
 
 
 
