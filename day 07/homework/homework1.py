7d4ecdc71b46b41681f04efb8cfff5fedc47461a
mateghviniashvili <ghviniashvilimate318@gmail.com>
2024-03-19 20:37:12 +0400

    commit

--- day 07/homework/homework1.py
+++ day 07/homework/homework1.py
@@ -1,7 +1,9 @@
-information = input("Enter your information: ")
-information1 = input("Enter your information 1: ")
-information2 = input("Enter your information2: ")
-information3 = input("enter your information 3: ")
+information = int(input("Enter your information: "))
+information1 = input("Enter your information: ")
+information2 = float(input("Enter your information2: "))
+information3 = input(type("Enteryour information3: "))
+
+
 
 
 
