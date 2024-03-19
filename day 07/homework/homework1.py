d297b13fb225c61ae886bd242ca48c80cadb832b
mateghviniashvili <ghviniashvilimate318@gmail.com>
2024-03-19 20:22:23 +0400

    completed homeworks

---
+++ day 07/homework/homework1.py
@@ -0,0 +1,10 @@
+information = input("Enter your information: ")
+information1 = input("Enter your information 1: ")
+information2 = input("Enter your information2: ")
+information3 = input("enter your information 3: ")
+
+
+
+
+
+
