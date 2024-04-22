b4ef4541da9b157d53a65e0789f3cd3c9d76b26a
mateghviniashvili <ghviniashvilimate318@gmail.com>
2024-04-22 23:44:58 +0400

    commit

---
+++ day 012/homework/homework12.py
@@ -0,0 +1,16 @@
+for i in range(1,11):
+   print(i)
+for i in range(11,0,-1):
+   print(i)
+for i in range(0,101,2):
+   print(i)
+for i in range(99,0,-2):
+  print(i)
+number = 0
+for i in range(1,11):
+ number += i
+print(number)
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+for i in range(a,b):
+  print(i)
---
+++ day 012/homework/lesson12.py
@@ -0,0 +1,30 @@
+i = 1
+while i <= 10:
+   print(i)
+   i += 1
+
+i = 10
+while i >= 1:
+  print(i)
+  i += -1
+
+i = 0
+while i <= 100:
+    print(i)
+    i += 2
+
+i = 99
+while i >= 1:
+   print(i)
+   i -= 2
+
+number = 0
+i = 1
+while i <= 10:
+ number += 1
+print(number)
+
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+for i in range(a,b):
+  print(i)
