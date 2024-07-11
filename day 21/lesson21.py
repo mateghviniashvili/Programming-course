1c33be0462077ea33a2f5000fef0eb7e1bcf97b2
mateghviniashvili <ghviniashvilimate318@gmail.com>
2024-07-11 19:35:34 +0400

    commit

---
+++ day 21/lesson21.py
@@ -0,0 +1,16 @@
+def marks_check(marks):
+ if marks == 10:
+    print("your child is very good")
+
+ elif marks == 8 or marks == 9:
+      print("your child is good")
+
+ elif marks == 5:
+      print("your child is bad")
+
+ elif marks < 5:
+      print("your child is horible")
+
+ else:
+
+  print("kick child out of school")
